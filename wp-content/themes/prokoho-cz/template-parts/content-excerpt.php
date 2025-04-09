c605482e4811ebe83d0c1c4042d53ad7bd3b9fde
richard03 <richard@prokoho.cz>
2025-04-09 12:16:47 +0200

    Excertpt with thumbnail

--- wp-content/themes/prokoho-cz/template-parts/content-excerpt.php
+++ wp-content/themes/prokoho-cz/template-parts/content-excerpt.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying posts in the loop with custom box format
+ * The template for displaying articles in the loop with post excerpts and featured image
  *
  * @package Prokoho_CZ
  */
@@ -8,12 +8,20 @@
 ?>
 
 <div class="custom-box custom-box-h25 custom-box-first">
-    <h3 class="custom-box-title">
-        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-    </h3>
-    <div class="custom-box-content">
+   <div class="custom-box-content">
+        <?php if (has_post_thumbnail()) : ?>
+            <div class="post-thumbnail">
+                <a href="<?php the_permalink(); ?>">
+                    <?php the_post_thumbnail('medium', array('style' => 'max-width: 100%; height: auto;')); ?>
+                </a>
+            </div>
+        <?php endif; ?>
+        <h3 class="custom-box-title">
+            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+        </h3> 
         <?php the_excerpt(); ?>
     </div>
+    
     <div class="custom-box-footer">
         <a href="<?php the_permalink(); ?>">Více o kurzu…</a>
     </div>
